7d2b9b4e36669e3fa824f38aae8b6d02e4876c0c
felipe <fortes.felipe95@gmail.com>
2022-10-13 15:40:30 -0300

    Requisito 04

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -16,8 +16,12 @@ const listById = async (req, res) => {
 const createProduct = async (req, res) => {
   const { name } = req.body;
 
-  if (name === '') {
-    return res.status(422).json({ message: 'O campo de nome não pode ser vazio' }); 
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' }); 
+  }
+
+  if (name.length < 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
   }
 
   const newProduct = await productService.createProduct(name);
